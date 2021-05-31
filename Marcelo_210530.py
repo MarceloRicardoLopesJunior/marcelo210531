acb1675124f338cc348d02a45dd4a7436a7b504c
MarceloRicardoLopesJunior <84341271+MarceloRicardoLopesJunior@users.noreply.github.com>
2021-05-31 13:11:43 -0300

    Add files via upload

---
+++ Marcelo_210530.py
@@ -0,0 +1,27 @@
+#question2
+name="marcelo"
+#question3
+age=16
+#question4
+print(name+str(age))
+#question5
+last_name= "lopes"
+print(name+"\n"+last_name)
+#question6
+print(name+"\n\t"+last_name)
+#question7
+character="   sonic   "
+#question8
+print(character)
+#question9
+print(character+"!")
+#question10
+print(character.strip()+"!")
+#question11
+print(character.lstrip()+"!")
+#question12
+fruits=["apple","grape","mellon"]
+#question13
+print(fruits)
+#question14
+print(fruits[1])
